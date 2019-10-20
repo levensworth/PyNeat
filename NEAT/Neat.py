fix neural net computation graph

NEAT/Neat.py
@@ -3,6 +3,7 @@ from NEAT.Genome import Genome
 from NEAT.NodeGene import NodeGeneType, NodeGene
 from NEAT.ConnectionGene import ConnectionGene
 from NEAT.Specie import Specie
+from NEAT.NeuralNet import NeuralNet
 import numpy as np
 from math import inf
 from random import Random
@@ -42,6 +43,7 @@ def evolution(input_set, output_set, epochs=1000):
             specie = species_map[genome]
             # mayyyyybe this is wrong
             score = evaluate(genome, input_set, output_set)
+            print('score {}'.format(score))
             adjusted_score = score / len(specie.members)
             specie.add_adjusted_fitness(adjusted_score)
             specie.set_fitness(genome, adjusted_score)
@@ -119,7 +121,7 @@ def creat_genesis(inputs, outputs, size=10):
 
 def evaluate(genome, input_set, output_set):
     phenotype = generate_phenotype(genome)
-    gene_fitness = get_fitness(phenotype, input_set, output_set)
+    gene_fitness = get_fitness_mse(phenotype, input_set, output_set)
     return gene_fitness
 
 
@@ -168,12 +170,12 @@ def select_genome_roulette(specie):
 
 def generate_phenotype(gene):
 
-    return gene
+    return NeuralNet(gene)
 
 
 def get_fitness(phenotype, input_set, output_set):
     """ for testing purposes we try to make topolgies gain 100 neurons"""
-    return len(phenotype.nodes)
+    return len(phenotype.predict(input_set))
 
 
 def get_fitness_mse(phenotype, input_set, output_set):
@@ -185,14 +187,17 @@ def get_fitness_mse(phenotype, input_set, output_set):
     :return: a float representing the inverse of MSE
     """
     predictions = phenotype.predict(input_set)
-    error = np.abs(output_set - predictions) ** 2
+    error = (output_set - predictions) ** 2
     error = np.sum(error) / len(output_set)
     return 1 / error if error != 0 else inf
 
 
 if __name__ == '__main__':
-    input_set = np.zeros([1, 2])
-    output_set = np.zeros([1, 1])
-    generation = evolution(input_set, output_set, 400)
+    input_set = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
+    output_set = np.array([[0], [1], [1], [1]])
+    generation = evolution(input_set, output_set, 200)
     for gene in generation:
-        print(len(gene.nodes))
+        phenotype = NeuralNet(gene)
+        print(phenotype.predict(input_set))
+        print(len(gene.connections))
+        print('--------')

NEAT/NeuralNet.py
@@ -1,9 +1,12 @@
 import math
 from NEAT.NodeGene import NodeGeneType
-
+import numpy as np
 
 def sigmoid(x, treshold):
-    return math.exp(x) / (math.exp(x)+1)
+    try:
+        return math.exp(x) / (math.exp(x)+1)
+    except OverflowError:
+        return float('inf')
 
 
 class NeuralNet(object):
@@ -21,15 +24,15 @@ class NeuralNet(object):
             nodes[node.get_id()] = Node(sigmoid, node.get_type(), node.get_id())
 
         for connection in gene.connections.values():
-            if not connection.enable:
-                break
-            in_node = nodes[connection.get_in_node()]
-            out_node = nodes[connection.get_out_node()]
+            if connection.enable:
 
-            in_node.add_out_node_connection(Connection(in_node, out_node, connection.weight
-                                                       , connection.innovation_number))
-            out_node.add_in_node_connection(Connection(in_node, out_node, connection.weight
-                                                       , connection.innovation_number))
+                in_node = nodes[connection.get_in_node()]
+                out_node = nodes[connection.get_out_node()]
+
+                in_node.add_out_node_connection(Connection(in_node, out_node, connection.weight
+                                                           , connection.innovation_number))
+                out_node.add_in_node_connection(Connection(in_node, out_node, connection.weight
+                                                           , connection.innovation_number))
         # we sort the keys to maintain the order between iterations
         # nodes = sorted(nodes, key=nodes.get)
         return nodes
@@ -37,8 +40,14 @@ class NeuralNet(object):
         # now we sort the list by type and sub sort by innovation number
 
     def predict(self, input_values):
+        result = []
+        for vec in input_values:
+            result.append(self._predict(vec))
+        return np.array(result)
+
+    def _predict(self, input_values):
         """
-        this method will try to redict an out given a numpy array input
+        this method will try to predict an out given a numpy array input
         :param input_values: numpy vector where each row represents a single input
         :return:
         """
@@ -51,12 +60,12 @@ class NeuralNet(object):
                 out_nodes.append(node)
 
         if len(input_values) != len(in_nodes):
-            raise AttributeError('input size is {} and should be {}'.format(input_values.shape[1], len(in_nodes)))
+            raise AttributeError('input size is {} and should be {}'.format(len(input_values), len(in_nodes)))
 
         # set the input vector
-        index = 0
+        col_index = 0
         for node in in_nodes:
-            node.receive_input(input_values[index])
+            node.receive_input(input_values[col_index])
 
         conn_stack = []
         for node in out_nodes:
@@ -79,11 +88,6 @@ class NeuralNet(object):
                 # we visit the connection
                 current_conn.visited = True
                 conn_stack.append(current_conn)
-                if current_conn.in_node.neuron_type == NodeGeneType.INPUT:
-                    # it's an input so we only get the input passed
-                    result = current_conn.in_node.input_val
-                    result = result * current_conn.weight
-                    current_conn.out_node.receive_input(result)
                 # and we append it's children
                 for conn in current_conn.in_node.in_nodes.values():
                     conn_stack.append(conn)
@@ -115,6 +119,8 @@ class Node(object):
         self.visited = False
 
     def process(self):
+        if self.neuron_type == NodeGeneType.INPUT:
+            return self.input_val
         return self.activation(self.input_val, self.threshold)
 
     def add_in_node_connection(self, connection):

NEAT/Tests/neural_net_test.py
@@ -13,13 +13,13 @@ def given_a_genome():
 
     nodes = {node1.get_id(): node1, node2.get_id(): node2, node3.get_id(): node3, node4.get_id(): node4,
              node5.get_id(): node5}
-    conn1 = ConnectionGene(node1.get_id(), node4.get_id(), 0.7, innovation_number=1)
+    conn1 = ConnectionGene(node1.get_id(), node4.get_id(), 0.5, innovation_number=1)
     conn2 = ConnectionGene(node2.get_id(), node4.get_id(), -0.5, False, 2)
     conn3 = ConnectionGene(node3.get_id(), node4.get_id(), 0.5, innovation_number=3)
-    conn4 = ConnectionGene(node2.get_id(), node5.get_id(), 0.2, innovation_number=4)
-    conn5 = ConnectionGene(node5.get_id(), node4.get_id(), 0.4, innovation_number=5)
-    conn6 = ConnectionGene(node1.get_id(), node5.get_id(), 0.6, innovation_number=6)
-    conn7 = ConnectionGene(node4.get_id(), node5.get_id(), 0.6, False, innovation_number=11)
+    conn4 = ConnectionGene(node2.get_id(), node5.get_id(), 0.5, innovation_number=4)
+    conn5 = ConnectionGene(node5.get_id(), node4.get_id(), 0.5, innovation_number=5)
+    conn6 = ConnectionGene(node1.get_id(), node5.get_id(), 0.5, innovation_number=6)
+    conn7 = ConnectionGene(node4.get_id(), node5.get_id(), 0.5, False, innovation_number=11)
 
     connections = {conn1.get_innovation_number(): conn1, conn2.get_innovation_number(): conn2,
                    conn3.get_innovation_number(): conn3, conn4.get_innovation_number(): conn4,
@@ -32,8 +32,8 @@ def given_a_genome():
 
 
 def given_an_input_out_set():
-    x = [1,1,1]
-    y = [0.7706068548532662]
+    x = [1, 1, 1]
+    y = [0.796656882614653]
 
     return np.array(x), np.array(y)
 
@@ -48,7 +48,9 @@ def test_input():
     net = NeuralNet.NeuralNet(genome)
     x, y = given_an_input_out_set()
     result = net.predict(x)
-    print( result == y)
+    print(result == y)
+    print(result)
+    print(y)
 
 
 if __name__ == '__main__':